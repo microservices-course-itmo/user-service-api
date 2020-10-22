f230c07eb8050a556f897941f90d612ce2867bbf
brovinnick <brovinnick@gmail.com>
2020-10-22 13:28:21 +0300

    add wine response dto

--- src/main/java/com/wine/to/up/user/service/api/dto/WineResponse.java
+++ src/main/java/com/wine/to/up/user/service/api/dto/WineResponse.java
@@ -1,5 +1,6 @@
 package com.wine.to.up.user.service.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,9 +8,10 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class WineResponse {
     private Long wineId;
     private String wineName;
-    private Long newWinePrice;
+    private Float newWinePrice;
     private List<UserTokens> userTokens;
 }
