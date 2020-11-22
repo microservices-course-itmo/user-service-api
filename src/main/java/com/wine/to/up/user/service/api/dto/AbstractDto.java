e05bab7add1160529bba4362cce40f3dd7bc020a
brovinnick <brovinnick@gmail.com>
2020-11-22 19:16:51 +0300

    Add DTO and Subscription Service

---
+++ src/main/java/com/wine/to/up/user/service/api/dto/AbstractDto.java
@@ -0,0 +1,11 @@
+package com.wine.to.up.user.service.api.dto;
+
+/**
+ * Marker interface.
+ * Used as a constraint.
+ * Needs to be implemented in all dto classes.
+ *
+ * @param <I> Type of ID field
+ */
+public interface AbstractDto<I> {
+}
