137b261c6817ed4f44a9c0a4f884bc34bde0c424
Kot1que <me@krvd.ru>
2021-02-28 20:58:20 +0300

    Feign client for favorites list

---
+++ src/main/java/com/wine/to/up/user/service/api/feign/FavoritesServiceClient.java
@@ -0,0 +1,26 @@
+package com.wine.to.up.user.service.api.feign;
+
+import com.wine.to.up.user.service.api.dto.ItemDto;
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+
+import java.util.List;
+
+public interface FavoritesServiceClient {
+    /**
+     * Returns list of user's favorites as list of ids
+     * @param token token provided in request
+     */
+    @RequestLine(value = "GET /favorites/list")
+    @Headers({"Content-Type: application/json", "Authorization: {token}"})
+    List<String> findUsersFavoritesIdList(@Param("token") String token);
+
+    /**
+     * Returns list of user's favorites
+     * @param token token provided in request
+     */
+    @RequestLine(value = "GET /favorites")
+    @Headers({"Content-Type: application/json", "Authorization: {token}"})
+    List<ItemDto> findUsersFavorites(@Param("token") String token);
+}
